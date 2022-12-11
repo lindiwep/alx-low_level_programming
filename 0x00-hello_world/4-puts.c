c87456d5ff2126b7ddf57e1d92db3e85fbb47f37
Precious <sambop42@gmail.com>
2022-12-11 12:33:23 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/**
+ *_main - print string in the put function
+ * 
+ *_Description : using main function
+ *_This programme prints "Programming is like building a multilingual puzzle"
+ *_Return (0)
+ */
+_int main(void)
+{
+	puts("\n" "Progrmming is like building a multilingual puzzle");
+	return (0)
+}
